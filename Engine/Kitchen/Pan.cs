21d79fe134826e320ab60af10dad3137541d31a5
m4ite <maitefeld2302@gmail.com>
2023-07-21 16:48:26 -0300

    panLoader

---
+++ Engine/Kitchen/Pan.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using Engine.Sprites;
+
+
+namespace Engine;
+
+using Sprites;
+
+using static ProjectPaths;
+
+public class Pan : Interactable
+{
+    public Pan(Rectangle r, PanTypes type) : base(new Rectangle(r.Location, new(48, 48)))
+    {
+        Loader = new PanSpriteLoader();
+        SpriteStream = Loader.GetAnimation(type);
+    }
+
+    Image panImage = Image.FromFile(AssetsPath + "pan3x.png");
+    SpriteStream SpriteStream { get; set; }
+    SpriteLoader<PanTypes> Loader { get; set; }
+
+    public override void Interact(Player p)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Draw(Graphics g)
+    {
+        var c = SpriteStream.Sprites.Last();
+
+        g.DrawImage(
+            panImage,
+            this.Box,
+            c.X,
+            c.Y,
+            c.Width,
+            c.Height,
+            GraphicsUnit.Pixel
+            );
+    }
+}
+
---
+++ Engine/Sprite/PanSprites/PanSpriteController.cs
@@ -0,0 +1,90 @@
+﻿using System.Drawing;
+
+namespace Engine.Sprites;
+using Extensions;
+
+
+
+
+
+
+
+public class PanSpriteLoader : SpriteLoader<PanTypes>
+{
+    public PanSpriteLoader() : base(scale:3) { }
+
+    Size SpriteSize = new(16,16);
+    protected override void Load()
+    {
+        var scaled = SpriteSize.Scale(this.Scale);
+
+        int startY = 0;
+
+        var voidPan = new SpriteStream();
+        var tomatoPan = new SpriteStream();
+        var onionPan = new SpriteStream();
+        var tomOnionPan = new SpriteStream();
+
+        for (int i = 0; i<scaled.Width * 5; i += scaled.Width)
+        {
+            var rect = new Rectangle(i, startY, scaled.Width, scaled.Height);
+            var sprite = new Sprite(rect.Location, rect.Size);
+            voidPan.Add(sprite);
+        }
+        this.Animations.Add(PanTypes.Void, voidPan);
+
+        startY += scaled.Width + 9;
+        
+        for (int i = 0; i<scaled.Width * 5; i += scaled.Width)
+        {
+            var rect = new Rectangle(i, startY, scaled.Width, scaled.Height);
+            var sprite = new Sprite(rect.Location, rect.Size);
+            tomatoPan.Add(sprite);
+        }
+        this.Animations.Add(PanTypes.TomatoPan, tomatoPan);
+
+        startY += scaled.Width + 9;
+
+        for(int i = 0;i<scaled.Width * 5;i += scaled.Width)
+        {
+            var rect = new Rectangle(i, startY, scaled.Width, scaled.Height);
+            var sprite = new Sprite(rect.Location, rect.Size);
+            onionPan.Add(sprite);
+        }
+        this.Animations.Add(PanTypes.OnionPan, onionPan);
+
+        startY += scaled.Width + 9;
+
+        for (int i = 0; i < scaled.Width * 5; i += scaled.Width)
+        {
+            var rect = new Rectangle(i, startY, scaled.Width, scaled.Height);
+            var sprite = new Sprite(rect.Location, rect.Size);
+            tomOnionPan.Add(sprite);
+        }
+        this.Animations.Add(PanTypes.TomOnionPan, tomOnionPan);
+
+    }
+}
+
+
+public class PanSpriteController
+    : SpriteController<PanSpriteLoader, PanTypes>
+{
+    public PanSpriteController()
+    {
+        this.SpriteLoader = new PanSpriteLoader();
+
+    }
+}
+
+public enum PanTypes
+{ 
+    Void,
+    TomatoPan,
+    OnionPan,
+    TomOnionPan
+}
+
+
+
+
