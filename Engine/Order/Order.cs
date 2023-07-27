0739d36713b223bb3d617c4138adc85af72fd01d
BrnCalisario <bruno145224@gmail.com>
2023-07-27 09:34:01 -0300

    Fixing plate ordering

---
+++ Engine/Order/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine;
+
+internal class Order
+{
+}
+
+public enum OrderType : byte
+{
+    TomatoSoup =1 ,
+    OnionSoup = 2,
+    MixedSoup = 4,
+    Steak = 8,
+    Fish = 16,
+    InvalidOrder = 32,
+}
+
+public static class OrderExtensions
+{
+    public static bool IsProtein(this OrderType order)
+        => order == OrderType.Steak || order == OrderType.Fish;
+}
