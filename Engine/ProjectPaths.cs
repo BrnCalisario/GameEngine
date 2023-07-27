3c2f35971d1a07bb88e70206eab3c073b6724b1d
m4ite <maitesenaibosch@gmail.com>
2023-07-27 15:48:53 -0300

    order sprite controller

--- Engine/ProjectPaths.cs
+++ Engine/ProjectPaths.cs
@@ -3,7 +3,7 @@ namespace Engine;
 public static class ProjectPaths
 {
     
-    public static string RootPath { get; } = "C:\\Users\\disrct\\Desktop\\GameEngine";
+    public static string RootPath { get; } = "C:\\Users\\Aluno\\Desktop\\GameEngine";
 
     public static string AssetsPath { get;} = RootPath + "\\assets\\";
 
---
+++ Engine/Sprite/OrderSprites/OrderSpriteController.cs
@@ -0,0 +1,80 @@
+﻿using System.Drawing;
+
+namespace Engine.Sprites;
+
+using Extensions;
+using static System.Windows.Forms.AxHost;
+
+
+public class OrderSpriteLoader : SpriteLoader<OrderTypes>
+{
+    public OrderSpriteLoader() : base(scale: 3) { }
+
+    Size SpriteSize = new(130, 130);
+
+    protected override void Load()
+    {
+        var scaled = SpriteSize.Scale(this.Scale);
+        int startX = 0;
+
+        var tomatoSoup = new SpriteStream();
+        var onionSoup = new SpriteStream();
+        var mixedSoup = new SpriteStream();
+        var steak = new SpriteStream();
+        var fish = new SpriteStream();
+
+        var rect = new Rectangle(startX, 0, scaled.Width, scaled.Height);
+        var sprite = new Sprite(rect.Location, rect.Size);
+        tomatoSoup.Add(sprite);
+        this.Animations.Add(OrderTypes.TomatoSoup, tomatoSoup);
+
+        startX += scaled.Width;
+
+        rect = new Rectangle(startX, 0, scaled.Width, scaled.Height);
+        sprite = new Sprite(rect.Location, rect.Size);
+        mixedSoup.Add(sprite);
+        this.Animations.Add(OrderTypes.MixedSoup, mixedSoup);
+
+        startX += scaled.Width;
+
+        rect = new Rectangle(startX, 0, scaled.Width, scaled.Height);
+        sprite = new Sprite(rect.Location, rect.Size);
+        onionSoup.Add(sprite);
+        this.Animations.Add(OrderTypes.OnionSoup, onionSoup);
+
+        startX += scaled.Width;
+
+        rect = new Rectangle(startX, 0, scaled.Width, scaled.Height);
+        sprite = new Sprite(rect.Location, rect.Size);
+        steak.Add(sprite);
+        this.Animations.Add(OrderTypes.Steak, steak);
+
+        startX += scaled.Width;
+
+        rect = new Rectangle(startX, 0, scaled.Width, scaled.Height);
+        sprite = new Sprite(rect.Location, rect.Size);
+        fish.Add(sprite);
+        this.Animations.Add(OrderTypes.Fish, fish);
+
+    }
+}
+
+
+public class OrderSpriteController
+    : SpriteController<OrderSpriteLoader, OrderTypes>
+{
+    public OrderSpriteController()
+    {
+        this.SpriteLoader = new OrderSpriteLoader();
+    }
+}
+
+
+public enum OrderTypes
+{ 
+    TomatoSoup,
+    OnionSoup,
+    MixedSoup,
+    Steak,
+    Fish
+}
